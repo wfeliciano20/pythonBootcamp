0a74ca4a8c487672670d07de2e30860b4a9f5898
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-06-27 21:01:36 -0400

    number operations

---
+++ PythonObjectandDataStructuresBasics/numbers.py
@@ -0,0 +1,29 @@
+# Adition
+print(2+1)
+
+# Subtraction
+print(2 - 1)
+
+# Multiplication
+print(2 * 2)
+
+# Division
+print(3/2)
+
+# Floor Division
+print(7 // 4)
+
+# Modulo
+print(7 % 4)
+
+# Powers
+print(2**3)
+
+# Can also do roots this way
+print(4**0.5)
+
+# Order of Operations followed in Python
+print(2 + 10 * 10 + 3)
+
+# Can use parentheses to specify orders
+print((2 + 10) * (10 + 3))
