b669ee0fe17a7d97cc4f79bb9c2163162c953619
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-08-01 15:14:07 -0400

    numbers and strings

---
+++ IntroToPython/numbers.py
@@ -0,0 +1,17 @@
+integer_division = 13//5
+print(integer_division)
+
+
+remainder = 13 % 5
+print(remainder)
+
+
+x = 37
+remainder = x % 2
+print(remainder)
+
+age = 34
+
+print("You are " + str(age))
+
+print(f"You are {age}")
---
+++ IntroToPython/stringFormatting.py
@@ -0,0 +1,8 @@
+name = "William"
+final_greating = "How are you, {}?"
+william_greeting = final_greating.format(name)
+
+
+name = "Will"
+final_greating2 = "How are you, {name}?"
+will_greating = final_greating2.format(name="Will")
