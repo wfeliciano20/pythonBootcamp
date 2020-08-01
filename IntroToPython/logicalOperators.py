cb6a4573622108a87746531d0a291e17d5de5e03
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-08-01 16:24:45 -0400

    logical op

---
+++ IntroToPython/logicalOperators.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+can_learn_programming = age > 0 and age < 150
+
+print(f"You can learn programming: {can_learn_programming}.")
+
+val = True
+
+greeting = False or val
+
+print(greeting)
