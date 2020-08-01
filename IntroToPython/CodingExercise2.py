920f38d0ce15e698e875699074600045bde0aa73
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-08-01 15:48:23 -0400

    exercise2 and update string formatting

---
+++ IntroToPython/CodingExercise2.py
@@ -0,0 +1,6 @@
+NAME = input("Enter your name: ")
+greeting = "Hello, {}".format(NAME)
+print(greeting)
+
+months_num = int(input("Enter your age: ")) * 12
+print("{}".format(months_num))
--- IntroToPython/stringFormatting.py
+++ IntroToPython/stringFormatting.py
@@ -1,8 +1,12 @@
-name = "William"
-final_greating = "How are you, {}?"
-william_greeting = final_greating.format(name)
+# name = "William"
+# final_greating = "How are you, {}?"
+# william_greeting = final_greating.format(name)
 
 
-name = "Will"
-final_greating2 = "How are you, {name}?"
-will_greating = final_greating2.format(name="Will")
+# final_greating2 = "How are you, {name}?"
+# will_greating = final_greating2.format(name="Will")
+# print(will_greating)
+
+# age = input("Enter your age: ")
+# age_num = int(age)
+# print(f'You have lived for {age_num * 12} months')
