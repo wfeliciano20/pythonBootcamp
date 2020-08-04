6731f53f2c6da86256e4940697e1ff702c4758e6
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-08-03 20:28:44 -0400

    edit conditional comprehensions

--- PythonFundamentals/comprehensionsWithConditionals.py
+++ PythonFundamentals/comprehensionsWithConditionals.py
@@ -9,7 +9,9 @@ guests = ["jose", "Bob", "Rolf", "Charlie", "michael"]
 friends_lower = [f.lower() for f in friends]
 
 present_friends = [
-    name.title() for name in guests if name.lower() in friends_lower
+    name.title()
+    for name in guests
+    if name.lower() in friends_lower
 ]
 
 print(present_friends)
