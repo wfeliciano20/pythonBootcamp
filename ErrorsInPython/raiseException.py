b383e53ba4db379fd00a64585865c995286b70d4
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-08-08 17:13:03 -0400

    raising errors

---
+++ ErrorsInPython/raiseException.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def __repr__(self):
+        return f'<Car {self.make} {self.model}>'
+
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def add_car(self, car):
+        if not isinstance(car, Car):
+            raise TabError(
+                F'Tried to add a `{car.__class__.__name__}` to the garage, but you can only add `Car` objects.')
+        self.cars.append(car)
